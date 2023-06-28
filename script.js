92408c7e5fe870e0fe3dfa98dcbf691619e20d91
turjoarts <129817382+turjoarts@users.noreply.github.com>
2023-06-28 20:59:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function sendEmail(){
+    var id=document.getElementById("p_id").value;
+    if(id=='p123')
+    {      
+        var ele = document.getElementsByName("rate");
+        var x;
+        for (i = 0; i < ele.length; i++) {
+            if (ele[i].checked)
+                x= ele[i].value;
+        }
+
+            Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "turjoarts@gmail.com",
+            Password : "29B5F905C9FF1DF589693C13CEB406A27D1B",
+            To : 'turjoarts@gmail.com',
+            From : "turjoarts@gmail.com",
+            Subject : "New Review From Customer",
+            Body : "Name: " + document.getElementById("cname").value 
+            + "<br> Rating: " + x 
+            +"<br> Description: "+ document.getElementById("describe").value
+        }).then(
+        message => alert("Review Sent Succesfully")
+        );
+    }
+    else
+    {
+        alert("Pleae Check Product ID")
+    }
+}
