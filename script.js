153383f3b7c948f81c3104f22d1c767bcf0362a7
turjoarts <129817382+turjoarts@users.noreply.github.com>
2023-06-29 10:03:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 
-function sendEmail(){
+function sendEmail()
+{
     var id=document.getElementById("p_id").value;
     if(id=='122333')
     {      
         var ele = document.getElementsByName("rate");
         var x;
-        for (i = 0; i < ele.length; i++) {
+        for (i = 0; i < ele.length; i++) 
+        {
             if (ele[i].checked)
                 x= ele[i].value;
         }
 
-            Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "turjoarts@gmail.com",
-            Password : "29B5F905C9FF1DF589693C13CEB406A27D1B",
-            To : 'turjoarts@gmail.com',
-            From : "turjoarts@gmail.com",
-            Subject : "New Review From Customer",
-            Body : "Name: " + document.getElementById("cname").value 
-            + "<br> Rating: " + x 
-            +"<br> Description: "+ document.getElementById("describe").value
-        }).then(
-        message => alert("Review Sent Succesfully")
-        );
+        if(x<=5)
+        {
+            Email.send(
+            {
+                Host : "smtp.elasticemail.com",
+                Username : "turjoarts@gmail.com",
+                Password : "29B5F905C9FF1DF589693C13CEB406A27D1B",
+                To : 'turjoarts@gmail.com',
+                From : "turjoarts@gmail.com",
+                Subject : "New Review From Customer",
+                Body : "Name: " + document.getElementById("cname").value 
+                + "<br> Rating: " + x 
+                +"<br> Description: "+ document.getElementById("describe").value
+            }).then(
+            message => alert("Review Sent Succesfully"));
+        }
+        else
+        {
+            alert("please give star");
+        }
+
     }
     else
     {
-        alert("Pleae Check Product ID")
+        alert("Pleae Check Product ID");
     }
 }
+
